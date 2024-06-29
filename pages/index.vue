eab6d7786d3155b7558d48252d9dd8981a6c18b6
privatslabs <privatslabs@outlook.fr>
2024-06-29 09:35:32 +0000

    toogle button integrer

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,16 @@
   <div class="wrapper">
     <div class="row">
       <div class="col-xl-4">
+
+        <label class="ui-switch">
+          <input type="checkbox" />
+          <div class="slider">
+            <div class="circle"></div>
+          </div>
+        </label>
+
+        <br>
+
         <img class="profil" src="/unnamed.jpg" width="250" /> <br />
         <br />
         <h4 class="nom">Privat Kouadio</h4>
@@ -143,13 +153,16 @@
             <div class="col-xl-6">
               <div>
                 <p class="legend">E-commerce site web</p>
-                <img src="/preview/preview-1.png" alt="" />
+                <a href="https://mshop-app.vercel.app/" target="_blanck">
+                  <img src="/preview/preview-1.png" alt="" />
+                </a>
               </div>
             </div>
+
             <div class="col-xl-6">
               <div>
                 <p class="legend"></p>
-                <img src="/preview/preview-1.png" alt="" />
+                <img src="/preview/preview_3.png" alt="" />
               </div>
             </div>
           </div>
@@ -158,13 +171,7 @@
             <div class="col-xl-6">
               <div>
                 <p class="legend"></p>
-                <img src="/preview/preview-1.png" alt="" />
-              </div>
-            </div>
-            <div class="col-xl-6">
-              <div>
-              <p class="legend"></p>  
-                <img src="/preview/preview-1.png" alt="" />
+                <img src="/preview/preview_2.png" alt="" />
               </div>
             </div>
           </div>
@@ -173,8 +180,10 @@
         <br />
       </div>
     </div>
-    
-      <span style="text-align: center; margin: 10px auto 10px auto;">© 2024 Privat's labs, Inc.</span>
+
+    <span style="text-align: center; margin: 10px auto 10px auto"
+      >© 2024 Privat's labs, Inc.</span
+    >
   </div>
 </template>
 
@@ -191,7 +200,7 @@ export default {
   box-sizing: border-box;
 }
 
-.wrapper{
+.wrapper {
   display: flex;
   flex-direction: column;
 }
@@ -233,17 +242,17 @@ body {
   border-radius: 7px;
   padding: 15px;
 }
-.col-xl-8 .reseau-box {
-  border: 1px solid rgba(128, 128, 128, 0.349);
-  border-radius: 7px;
-  padding: 15px;
-}
+
 .col-xl-8 .col-xl-6 img {
-  width: 290px;
+  width: 250px;
   border: 1px solid;
   border-radius: 15px;
   cursor: pointer;
   box-shadow: 0 3px 9px rgba(142, 139, 139, 0.1); /* Ombre portée */
+}
+
+.col-xl-8 .col-xl-6 a {
+  color: black;
 }
 
 .tech img {
@@ -260,6 +269,8 @@ body {
 
 
 
+
+/* ------------------------------------------------------------------------------------------------- */
 
 .social-buttons {
   display: flex;
@@ -339,8 +350,90 @@ body {
   fill: #c13584;
 }
 
+/* switch settings 👇 */
 
+.ui-switch {
+  /* switch */
+  --switch-bg: rgb(135, 150, 165);
+  --switch-width: 48px;
+  --switch-height: 20px;
+  /* circle */
+  --circle-diameter: 32px;
+  --circle-bg: rgb(0, 56, 146);
+  --circle-inset: calc((var(--circle-diameter) - var(--switch-height)) / 2);
+}
 
+.ui-switch input {
+  display: none;
+}
+
+.slider {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  width: var(--switch-width);
+  height: var(--switch-height);
+  background: var(--switch-bg);
+  border-radius: 999px;
+  position: relative;
+  cursor: pointer;
+}
+
+.slider .circle {
+  top: calc(var(--circle-inset) * -1);
+  left: 0;
+  width: var(--circle-diameter);
+  height: var(--circle-diameter);
+  position: absolute;
+  background: var(--circle-bg);
+  border-radius: inherit;
+  background-image: url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjAiIHdpZHRoPSIyMCIgdmlld0JveD0iMCAwIDIwIDIwIj4KICAgIDxwYXRoIGZpbGw9IiNmZmYiCiAgICAgICAgZD0iTTkuMzA1IDEuNjY3VjMuNzVoMS4zODlWMS42NjdoLTEuMzl6bS00LjcwNyAxLjk1bC0uOTgyLjk4Mkw1LjA5IDYuMDcybC45ODItLjk4Mi0xLjQ3My0xLjQ3M3ptMTAuODAyIDBMMTMuOTI3IDUuMDlsLjk4Mi45ODIgMS40NzMtMS40NzMtLjk4Mi0uOTgyek0xMCA1LjEzOWE0Ljg3MiA0Ljg3MiAwIDAwLTQuODYyIDQuODZBNC44NzIgNC44NzIgMCAwMDEwIDE0Ljg2MiA0Ljg3MiA0Ljg3MiAwIDAwMTQuODYgMTAgNC44NzIgNC44NzIgMCAwMDEwIDUuMTM5em0wIDEuMzg5QTMuNDYyIDMuNDYyIDAgMDExMy40NzEgMTBhMy40NjIgMy40NjIgMCAwMS0zLjQ3MyAzLjQ3MkEzLjQ2MiAzLjQ2MiAwIDAxNi41MjcgMTAgMy40NjIgMy40NjIgMCAwMTEwIDYuNTI4ek0xLjY2NSA5LjMwNXYxLjM5aDIuMDgzdi0xLjM5SDEuNjY2em0xNC41ODMgMHYxLjM5aDIuMDg0di0xLjM5aC0yLjA4NHpNNS4wOSAxMy45MjhMMy42MTYgMTUuNGwuOTgyLjk4MiAxLjQ3My0xLjQ3My0uOTgyLS45ODJ6bTkuODIgMGwtLjk4Mi45ODIgMS40NzMgMS40NzMuOTgyLS45ODItMS40NzMtMS40NzN6TTkuMzA1IDE2LjI1djIuMDgzaDEuMzg5VjE2LjI1aC0xLjM5eiIgLz4KPC9zdmc+");
+  background-repeat: no-repeat;
+  background-position: center center;
+  -webkit-transition: left 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, -webkit-transform 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  -o-transition: left 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, transform 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  transition: left 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, transform 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, -webkit-transform 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  box-shadow: 0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12);
+  ;
+}
+
+.slider .circle::before {
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: rgba(255, 255, 255, 0.75);
+  border-radius: inherit;
+  -webkit-transition: all 500ms;
+  -o-transition: all 500ms;
+  transition: all 500ms;
+  opacity: 0;
+}
+
+/* actions */
+
+.ui-switch input:checked+.slider .circle {
+  left: calc(100% - var(--circle-diameter));
+  background-image: url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjAiIHdpZHRoPSIyMCIgdmlld0JveD0iMCAwIDIwIDIwIj4KICAgIDxwYXRoIGZpbGw9IiNmZmYiCiAgICAgICAgZD0iTTQuMiAyLjVsLS43IDEuOC0xLjguNyAxLjguNy43IDEuOC42LTEuOEw2LjcgNWwtMS45LS43LS42LTEuOHptMTUgOC4zYTYuNyA2LjcgMCAxMS02LjYtNi42IDUuOCA1LjggMCAwMDYuNiA2LjZ6IiAvPgo8L3N2Zz4=");
+}
+
+.ui-switch input:active+.slider .circle::before {
+  -webkit-transition: 0s;
+  -o-transition: 0s;
+  transition: 0s;
+  opacity: 1;
+  width: 0;
+  height: 0;
+}
 
 @media only screen and (max-width: 1200px) {
   .col-xl-4 {
@@ -361,8 +454,8 @@ body {
     margin-bottom: 50px;
   }
   .col-xl-8 .top-projet {
-  border: none;
-  padding: 15px;
-}
+    border: none;
+    padding: 15px;
+  }
 }
 </style>
